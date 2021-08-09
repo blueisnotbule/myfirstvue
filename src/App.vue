da9ec29957ff0c9207f0e8b770e2d6091c788a50
blueisnotbule <2895702227@qq.com>
2021-08-09 19:19:45 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <main-tab-bar>
+  </div>
+</template>
+
+<script>
+import MainTarBarVue from "components/content/maintarbar/MainTarBar.vue";
+export default {
+  name: "App",
+  components: {
+    MainTarBarVue,
+  },
+};
+</script>
+
+<style>
+@import "assets/css/base.css";
+</style>
